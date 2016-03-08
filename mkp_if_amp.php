dfc66fa17f443c08aa78a18a62db141b2910b0cf
Michael K Pate <michaelkpate@gmail.com>
2016-03-07 20:23:50 -0500

    Create mkp_if_amp.php

---
+++ mkp_if_amp.php
@@ -0,0 +1,20 @@
+// TXP 4.6 tag registration
+if (class_exists('Textpattern_Tag_Registry')) {
+	Txp::get('Textpattern_Tag_Registry')
+		->register('adi_if_amp')
+	;
+}
+
+function mkp_if_amp($atts, $thing='')
+{
+
+	global $variable;
+
+	// preg_replace("|^https?://[^/]+|i", "", serverSet('REQUEST_URI')) was copied from the function preText in publish.php
+
+	$parts = explode('/', preg_replace("|^https?://[^/]+|i", "", serverSet('REQUEST_URI')), 5);
+
+	// if the url ends in 'amp' this will return true; otherwise false
+	 
+	return (end($parts) == 'amp') ? parse(EvalElse($thing, true)) : parse(EvalElse($thing, false));
+}
